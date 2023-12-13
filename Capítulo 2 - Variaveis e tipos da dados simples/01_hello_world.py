235885661d35184b06b7122433f16d7b437dd616
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-12-12 22:55:35 -0300

    Start em variáveis e tipo de dados simples

---
+++ Capítulo 2 - Variaveis e tipos da dados simples/01_hello_world.py
@@ -0,0 +1,15 @@
+# O que realmente ocorre  quando executamos o hello_world.py
+
+message = "Hello python world!"
+print(message)
+
+message = "Hello python Crash Course world!"
+print(message)
+
+# Nomeando e usando variáveis
+
+#- pode ter conter sometre letras, números e underscores
+#- não podem começar com números
+#- Não pode ter espaços no nome da variável
+#- Não utilizar palavras reservadas por fução do Python 
+#- Nomes de variáves devem ser sucintas e descritivas
---
+++ Capítulo 2 - Variaveis e tipos da dados simples/03_faca_voce_mesmo.py
@@ -0,0 +1,7 @@
+# 2.1 Mensagem simples -Atribua uma mensagem ea uma variável e a exiba
+variavel =  'Mostrando mensagem!!'
+print(variavel)
+
+#22 Mude  o valor da variável  para uma nova mensagem e mostre essa nova mensagem 
+variavel = 'Nova Mensagem atribuida'
+print(variavel)
