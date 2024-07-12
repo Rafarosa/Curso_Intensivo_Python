9bf018b2f11f49d8731f50031d582af1d90f5494
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2024-07-12 20:51:31 -0300

    Finalizando capitulo 2

---
+++ Capítulo 2 - Variaveis e tipos da dados simples/05_numeros.py
@@ -0,0 +1,22 @@
+""" 
+Faça você mesmo
+
+2.9 Número oito
+Escreva operaçoes de adição,subtraçào, multiplicação e divisão de resultem cada uma no número 8.
+Não se esqueça de incluir suas operaçoes em um print() para conferir os resultados.
+""" 
+print(5+3)
+print(12-4)
+print(80/10)
+print(4*2)
+
+""" 
+2.10 Número favorito : 
+Use uma variável para representar seu número favorito . 
+Em seguida , usando essa variável , crie uma mensagem que revele seu número favorito . 
+Printe essa mensagem .
+"""
+num_fav = 34
+msg = f'Meu número favorito é {num_fav}'
+
+print(msg)
---
+++ Capítulo 2 - Variaveis e tipos da dados simples/06_comentarios.py
@@ -0,0 +1,15 @@
+# Diga olá a todos
+print('Olá a todos!!')
+
+
+# Quais tipos de comentários você deve escrever?
+"""
+- Explicar o que seu código deve fazer e como você faz o código rodar.
+"""
+"""
+2.11 Adicionando comentários: 
+Escolha dois dos programas que você escreveu e adicione pelo menos um comentário a cada um. 
+Caso não tenha nada específico para escrever porque até agora seus programas são muito simples, 
+basta acrescentar seu nome e a data atual no início de cada arquivo do programa. 
+Em seguida, escreva uma frase descrevendo o que o programa faz
+"""
